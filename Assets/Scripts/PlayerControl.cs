d129f9083afbdb28dd9ad3861613c3da2b29211c
Ragnar <raggym833@gmail.com>
2018-08-18 11:29:31 -0700

    Second Commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+
+    // test
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
